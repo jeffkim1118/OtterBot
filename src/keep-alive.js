702689c8ddb9e24bff70e786c3caecef128e3492
Jeffrey <1019yskim@gmail.com>
2021-06-16 15:38:04 -0400

    heroku sleep prevention

--- src/keep-alive.js
+++ src/keep-alive.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
 setInterval(async() => {
-    await fetch([''])
+    await fetch(['https://otterbot-1.herokuapp.com/'])
     console.log('Prevented from sleeping')
 }, 5 * 60 * 1000);
